d8b84733e9928e0bcbaaa4261e85a905e374d7a8
Matthew Ngan <matthewngan3@gmail.com>
2024-10-10 21:45:29 +0800

    change to standalone mode

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 import withPWA from 'next-pwa';
 
 const nextConfig = {
-  // output: "standalone",
+  output: "standalone",
   swcMinify: true,            // Enable SWC minification for improved performance
   compiler: {
       removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
