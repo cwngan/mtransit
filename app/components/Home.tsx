cd1eed0ad286be9c399a4e7611a4bfb7d46b141a
Matthew Ngan <matthewngan3@gmail.com>
2025-01-17 14:36:23 +0800

    Fix build error

---
+++ app/components/Home.tsx
@@ -0,0 +1,64 @@
+import { useSearchParams } from "next/navigation";
+import CurrentTabContext from "../store/CurrentTabContext";
+import SearchBar from "./SearchBar";
+import Link from "next/link";
+import clsx from "clsx";
+import StationsNearbyList from "./StationsNearbyList";
+import FavoriteStationsList from "./FavoriteStationsList";
+
+export default function Home() {
+  const searchParams = useSearchParams();
+  const currentTab = parseInt(searchParams.get("tab") || "1");
+
+  return (
+    <CurrentTabContext.Provider value={currentTab}>
+      <div className="container mx-auto flex h-full max-h-full flex-col">
+        <SearchBar />
+        <div className={clsx("grid w-full grid-cols-2")}>
+          <Link href={"/?tab=0"}>
+            <div
+              className={clsx(
+                "rounded-t-xl p-4 pb-2 text-center leading-none",
+                currentTab === 0
+                  ? "bg-cyan-600 text-white"
+                  : "bg-white text-black",
+              )}
+            >
+              附近站點
+            </div>
+          </Link>
+          <Link href={"/?tab=1"}>
+            <div
+              className={clsx(
+                "rounded-t-xl p-4 pb-2 text-center leading-none",
+                currentTab === 1
+                  ? "bg-orange-600 text-white"
+                  : "bg-white text-black",
+              )}
+            >
+              收藏站點
+            </div>
+          </Link>
+        </div>
+        <div className="flex-grow">
+          <div
+            className={clsx(
+              currentTab === 0 ? "block" : "hidden",
+              "h-full rounded-tr-xl bg-cyan-600",
+            )}
+          >
+            <StationsNearbyList />
+          </div>
+          <div
+            className={clsx(
+              currentTab === 1 ? "block" : "hidden",
+              "h-full rounded-tl-xl bg-orange-600",
+            )}
+          >
+            <FavoriteStationsList />
+          </div>
+        </div>
+      </div>
+    </CurrentTabContext.Provider>
+  );
+}
